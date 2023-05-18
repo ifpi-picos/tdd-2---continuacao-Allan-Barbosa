b2f4fde05263ca18bd6d4d3fa027015e8bba5d99
Allan Barbosa <allanb3779@gmail.com>
2023-05-17 21:05:12 -0300

    alterando o app.js e concluindo a atividade

--- src/app.js
+++ src/app.js
@@ -5,20 +5,41 @@ app.use(express.json());
 const Tarefas = require('./tarefas');
 const tarefas = new Tarefas();
 
-app.post('/tarefas', (req, res) => {
+app.post('/tarefas', async (req, res) => {
+    try {
+        await tarefas.adicionar(req.body.descricao);
+        res.status(201).send('Tarefa adicionada com sucesso!');
+    } catch (e) {
+        res.status(400).send(e);
+    }
 
 });
 
-app.delete('/tarefas/:indice', (req, res) => {
-
+app.delete('/tarefas/:indice', async (req, res) => {
+try {
+        await tarefas.remover(req.params.indice);
+        res.status(200).send('Tarefa removida com sucesso!');
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
-app.get('/tarefas', (req, res) => {
-
+app.get('/tarefas', async (req, res) => {
+    try {
+        const listaTarefas = await tarefas.todas();
+        res.status(200).send(listaTarefas);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
-app.patch('/tarefas/:indice', (req, res) => {
-
+app.patch('/tarefas/:indice', async (req, res) => {
+    try {
+        await tarefas.marcarConcluida(req.params.indice);
+        res.status(200).send('Tarefa marcada como concluída!');
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 module.exports = app;
--- src/tarefas.js
+++ src/tarefas.js
@@ -1,24 +1,25 @@
 class Tarefas {
-    constructor() {
-      this.lista = [];
-    }
-  
-    adicionar(descricao) {
-      
-    }
-  
-    remover(indice) {
-      
-    }
-  
-    todas() {
-      
-    }
-  
-    marcarConcluida(indice) {
-     
-    }
+  constructor() {
+    this.lista = [];
   }
-  
+
+  adicionar(descricao) {
+    const tarefa = { descricao: descricao, concluida: false}
+    this.lista.push(tarefa)
+    return tarefa
+  }
+
+  remover(indice) {
+    this.lista.splice(indice)
+  }
+
+  todas() {
+    return this.lista;
+  }
+
+  marcarConcluida(indice) {
+   return this.lista[indice].concluida = true
+  }
+}
+
 module.exports = Tarefas;
-  
